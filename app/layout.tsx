7cba79c5d1a5d395bed216a82144ecdd4795c194
Saksham Basnet <108948291+sakshambasnet@users.noreply.github.com>
2024-10-22 11:49:39 +0545

    Athuntecation implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -12,12 +14,21 @@ export const metadata = {
   description: "A live documentation platform for your Next.js app",
 };
 
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+      variables: {colorPrimary: '#3371ff', fontSize: '16px'}
+    }}
+    >
+
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
@@ -25,5 +36,7 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
